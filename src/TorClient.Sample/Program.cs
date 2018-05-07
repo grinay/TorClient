a1f599398a1f2bf6587cc08096d7b2b88e62bf65
Paul Alves <phgalves@outlook.com>
2018-05-07 19:16:02 -0300

    adding a code sample to show how the client works basically.

---
+++ src/TorClient.Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TorClient.Sample {
+    using System;
+    using System.Threading.Tasks;
+    using global::TorClient.Options;
+
+    public class Program {
+        public static async Task Main(string[] args) {
+            var geoLocationServiceUrl = "http://ip-api.com/json/{0}?fields=country&lang=en";
+            using (var client = new TorClient(new TorOptions { Password = "<Your Password Here>" })) {
+                for (;;){
+                    await client.TorControl.RenewIpAddress();
+                    Console.WriteLine(await client.Http.GetStringAsync(string.Format(
+                        geoLocationServiceUrl,
+                        client.IpAddress))
+                    );
+                }
+            }
+        }
+    }
+}
